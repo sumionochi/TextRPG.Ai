7b64763b9ddd24ea60a12553efde0f6b32e47d2d
sumionochi <aaditya.srivastava.connect@gmail.com>
2024-02-18 21:49:29 +0530

    defined all the schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,5 +5,16 @@ export default defineSchema({
   entries: defineTable({
     input: v.string(),
     response: v.string(),
+    adventureId: v.id("adventures"),
+    imageUrl: v.optional(v.string()),
+    health: v.number(),
+    inventory: v.array(v.string()),
+  }),
+  adventures: defineTable({
+    characterClass: v.string(),
+  }),
+  items: defineTable({
+    itemName: v.string(),
+    imageUrl: v.optional(v.string()),
   }),
 });
--- src/components/Dashboardpage.tsx
+++ src/components/Dashboardpage.tsx
@@ -13,14 +13,14 @@ const Dashboardpage = (props: Props) => {
   const [message, setMessage] = useState("");
 
   return (
-    <main className='flex min-h-screen flex-row pt-24 px-4'>
+    <main className='flex max-w-6xl mx-auto min-h-screen flex-row pt-24 px-4'>
         <div className='flex flex-col w-1/2 gap-3'>
-            <div className='bg-secondary p-4 rounded-md'>
+            <div className='bg-secondary p-4 rounded-md overflow-y-auto h-[400px]'>
                 {entries?.map(entry=>{
                     return (
                         <div key={entry._id} className='flex flex-col gap-2'>
                             <div>{entry.input}</div>
-                            <div>{entry.response}</div>
+                            <div className='mb-4'>{entry.response}</div>
                         </div>
                     )
                 })}
@@ -28,6 +28,7 @@ const Dashboardpage = (props: Props) => {
             <form onSubmit={(e)=>{
                 e.preventDefault();
                 handlePlayerAction({message})
+                setMessage('');
             }} className='flex flex-row gap-2'> 
                 <Input name="message" className='p-1 rounded-md' value={message} onChange={(e)=>setMessage(e.target.value)}/>
                 <Button>Submit</Button>
